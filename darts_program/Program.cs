19deef1fff614ba0bc7133d76ed3bcbf5d8593da
broughtl <louisnumismatist@gmail.com>
2024-01-20 21:47:15 +0000

    Changed structure

--- darts_program/Program.cs
+++ darts_program/Program.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace darts_program;
 
-namespace darts_program
+internal class Program
 {
-  internal class Program
+  static void Main(string[] args)
   {
-    static void Main(string[] args)
-    {
-    }
   }
 }
