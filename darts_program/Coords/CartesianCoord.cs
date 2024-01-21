57773b547e7bacd1c5f79423192e2a924f69f60d
broughtl <louisnumismatist@gmail.com>
2024-01-21 00:28:08 +0000

    Created coordinate classes.

---
+++ darts_program/Coords/CartesianCoord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace darts_program.Coords;
+
+internal class CartesianCoord(double x, double y)
+{
+    public readonly double X = x;
+    public readonly double Y = y;
+
+    public PolarCoord ToPolar()
+    {
+        double distance = Math.Sqrt(X * X + Y * Y);
+        double angle = Math.Atan(Y / X);
+
+        return new PolarCoord(distance, angle);
+    }
+}
---
+++ darts_program/Coords/PolarCoord.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace darts_program.Coords;
+
+internal class PolarCoord(double distance, double angle)
+{
+    public readonly double Distance = distance;
+    public readonly double Angle = angle;
+
+    public CartesianCoord ToCartesian()
+    {
+        double x = Distance * Math.Cos(Angle);
+        double y = Distance * Math.Sin(Angle);
+
+        return new CartesianCoord(x, y);
+    }
+}
