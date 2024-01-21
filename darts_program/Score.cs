fbc8b3cfabb50f136de2d88f4acd2ebd1b9761f0
broughtl <louisnumismatist@gmail.com>
2024-01-21 00:28:42 +0000

    Update Score.cs

--- darts_program/Score.cs
+++ darts_program/Score.cs
@@ -1,5 +1,11 @@
 ﻿namespace darts_program;
 
-internal class Score
+internal class Score(Score.Multiplier mult, int value)
 {
+  public enum Multiplier { NONE, S, D, T };
+
+  public readonly Multiplier Mult = mult;
+  public readonly int Value = value;
+
+  public int GetScore() => (int)Mult * Value;
 }
