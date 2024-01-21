d2940e75b522c3c9a7c16e9a3611d5dd308bdff5
broughtl <louisnumismatist@gmail.com>
2024-01-21 10:45:22 +0000

    Added ToString() override.

--- darts_program/Score.cs
+++ darts_program/Score.cs
@@ -8,4 +8,30 @@ internal class Score(Score.Multiplier mult, int value)
   public readonly int Value = value;
 
   public int GetScore() => (int)Mult * Value;
+
+  public override string ToString()
+  {
+    string str;
+
+    switch(Mult)
+    {
+      case Multiplier.S:
+        str = "S";
+        break;
+      case Multiplier.T:
+        str = "T";
+        break;
+      case Multiplier.D:
+        str = "D";
+        break;
+      case Multiplier.NONE:
+      default:
+        str = "-";
+        break;
+    }
+
+    if (Value < 10) str += "0";
+
+    return str += Value;
+  }
 }
